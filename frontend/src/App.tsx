ab3ed4e041a94df35f8fe1d33d70d71cab10a68b
Gal <galc@wix.com>
2021-03-02 21:35:11 +0200

    improved project structure & added axios util

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import { get } from './utils/axios-utils/axiosUtils';
 
 function App() {
+
+  useEffect(() => {
+    get().then((res: any) => console.log(res));
+  }, [])
+
+
   return (
     <div className="App">
       <div>React App</div>
