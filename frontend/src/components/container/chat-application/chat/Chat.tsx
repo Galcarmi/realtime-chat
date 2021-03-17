71eeb4cb86e87d97c3a8e2f91399936af075e3fb
Gal <galc@wix.com>
2021-03-17 19:56:08 +0200

    added send button and message input effects

--- frontend/src/components/container/chat-application/chat/Chat.tsx
+++ frontend/src/components/container/chat-application/chat/Chat.tsx
@@ -1,12 +1,36 @@
 import React, {FC} from 'react';
 import Message from './message/Message';
-
+import s from './chat.module.css';
 interface ChatProps {
     className?: string;
 }
 const Chat:FC<ChatProps> = (props) => {
     return (
-        <div className={props.className}>
+        <div className={[props.className, s.chat].join(' ')}>
+            <Message content={'1sadasdasd@@'}></Message>
+            <Message content={'2asdasd asdascxzc adfqeqwe fdsfd a'} friendMessage={true}></Message>
+            <Message content={'3asdasd qwe as xzv svsd fad asdsad as'}></Message>
+            <Message content={'4 asd adfqed asc adf 1!!'}></Message>
+            <Message content={'1sadasdasd@@'}></Message>
+            <Message content={'2asdasd asdascxzc adfqeqwe fdsfd a'} friendMessage={true}></Message>
+            <Message content={'3asdasd qwe as xzv svsd fad asdsad as'}></Message>
+            <Message content={'4 asd adfqed asc adf 1!!'}></Message>
+            <Message content={'1sadasdasd@@'}></Message>
+            <Message content={'2asdasd asdascxzc adfqeqwe fdsfd a'} friendMessage={true}></Message>
+            <Message content={'3asdasd qwe as xzv svsd fad asdsad as'}></Message>
+            <Message content={'4 asd adfqed asc adf 1!!'}></Message>
+            <Message content={'1sadasdasd@@'}></Message>
+            <Message content={'2asdasd asdascxzc adfqeqwe fdsfd a'} friendMessage={true}></Message>
+            <Message content={'3asdasd qwe as xzv svsd fad asdsad as'}></Message>
+            <Message content={'4 asd adfqed asc adf 1!!'}></Message>
+            <Message content={'1sadasdasd@@'}></Message>
+            <Message content={'2asdasd asdascxzc adfqeqwe fdsfd a'} friendMessage={true}></Message>
+            <Message content={'3asdasd qwe as xzv svsd fad asdsad as'}></Message>
+            <Message content={'4 asd adfqed asc adf 1!!'}></Message>
+            <Message content={'1sadasdasd@@'}></Message>
+            <Message content={'2asdasd asdascxzc adfqeqwe fdsfd a'} friendMessage={true}></Message>
+            <Message content={'3asdasd qwe as xzv svsd fad asdsad as'}></Message>
+            <Message content={'4 asd adfqed asc adf 1!!'}></Message>
             <Message content={'1sadasdasd@@'}></Message>
             <Message content={'2asdasd asdascxzc adfqeqwe fdsfd a'} friendMessage={true}></Message>
             <Message content={'3asdasd qwe as xzv svsd fad asdsad as'}></Message>
